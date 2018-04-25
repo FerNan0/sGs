6816ba30773b7379f6318849cf9e1873a075895d
Fernando F Duarte <feernando.duarte@gmail.com>
2018-04-25 18:57:49 -0300

    agora vai

---
+++ sGs/sGs/PrincipalViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PrincipalViewController.swift
+//  sGs
+//
+//  Created by Fernando F Duarte on 25/04/18.
+//  Copyright © 2018 Fernando F Duarte. All rights reserved.
+//
+
+import Foundation
